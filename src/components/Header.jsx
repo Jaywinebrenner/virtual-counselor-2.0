c1016ecb4009070750ac38d32b59f53d2373d7bd
Student <example@example.com>
2020-03-10 14:41:52 -0700

    tinker with css body and about page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,7 +28,7 @@ const Header = (props) => {
     <FaRegHandshake className='left-hand-icon'/>
 
     <Switch>
-      <NavLink  className='navbar-about-text' onClick={props.onFadeOut} onMouseOut={removeChange} style={{ textDecoration: 'none', fontSize: '1.3rem', marginBottom: '90px', marginRight: '5rem',fontWeight: 'bold' }} to="/">Home</NavLink>
+      <NavLink  className='navbar-about-text' onClick={props.onFadeOut} onMouseEnter={removeChange} style={{ textDecoration: 'none', fontSize: '1.3rem', marginBottom: '90px', marginRight: '5rem',fontWeight: 'bold' }} to="/">Home</NavLink>
     </Switch>
 
     <NavLink  className='navbar-about-text' onMouseOut={removeChange} className='navbar-about-text' style={{ textDecoration: 'none', fontSize: '1.3rem',marginBottom: '90px', marginRight: '3rem',fontWeight: 'bold' }}  to="/about">About</NavLink>
